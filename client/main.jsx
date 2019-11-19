dc624c81d2f84c9d0ae132364d86a312753bd38c
Patrick Schubert <patrick.schubert1@gmail.com>
2019-11-19 17:02:11 +0100

    .

--- client/main.jsx
+++ client/main.jsx
@@ -4,18 +4,31 @@ import React from 'react';
 import gql from 'graphql-tag';
 
 import ApolloClient from 'apollo-client';
+// import { OfflineClient } from 'offix-client';
 import { DDPLink } from 'meteor/swydo:ddp-apollo';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
 
 import App from '/imports/ui/App';
 
+// OFFIX:
+// https://offix.dev/docs/client-configuration
+// const offlineClient = new OfflineClient({
+//   terminatingLink: new DDPLink(),
+//   cache: new InMemoryCache()
+// });
+// export let client;
+
+// NORMAL:
 export const client = new ApolloClient({
   link: new DDPLink(),
   cache: new InMemoryCache()
 });
+window.client = client;
 
-Meteor.startup(() => {
+Meteor.startup(async () => {
+  // client = await offlineClient.init();
+  // console.log('APOLLO CLIENT:', client);
   render(
     <ApolloProvider client={client}>
       <App />
@@ -30,19 +43,19 @@ Meteor.startup(() => {
  * A more advanced React developer might be able to help put this where it should
  */
 
-const SUBSCRIBE_USER_CHANGES = gql`
-  subscription {
-    user {
-      event
-      doc
-    }
-  }
-`;
-const observer = client.subscribe({
-  query: SUBSCRIBE_USER_CHANGES,
-});
-const subscription = observer.subscribe({
-  next({ data }) {
-    console.log('main.jsx Subscription data:', data);
-  },
-});
+// const SUBSCRIBE_USER_CHANGES = gql`
+//   subscription {
+//     user {
+//       event
+//       doc
+//     }
+//   }
+// `;
+// const observer = client.subscribe({
+//   query: SUBSCRIBE_USER_CHANGES,
+// });
+// const subscription = observer.subscribe({
+//   next({ data }) {
+//     console.log('main.jsx Subscription data:', data);
+//   },
+// });
