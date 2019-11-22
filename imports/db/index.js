e1895729f3c24baab8122bb67be4730adcc0bc69
Patrick Schubert <patrick.schubert1@gmail.com>
2019-11-22 15:14:49 +0100

    grounddb :)

--- imports/db/index.js
+++ imports/db/index.js
@@ -14,14 +14,24 @@ export {
 }
 
 if(Meteor.isClient) {
+  // https://github.com/Twisterking/grounddb/tree/master
   [Groups].forEach(collection => {
-    // https://github.com/Twisterking/grounddb/tree/master
     const groundColl = new Ground.Collection(`_${collection._name}`);
     groundColl.observeSource(collection.find({}));
     ['find', 'findOne'].forEach(method => {
-      if(Tracker.nonreactive(() => Meteor.status().status == "offline")) {
-        collection[method] = groundColl[method];
+      const origMethod = collection[method];
+      collection[method] = function(...args) {
+        if(Tracker.nonreactive(() => Meteor.status().status == "offline")) {
+          console.log('OFFLINE! ' + method);
+          console.log('current Data:', groundColl.find({}).fetch());
+          return groundColl[method].call(this, ...args);
+        }
+        console.log('ONLINE! ' + method);
+        return origMethod.call(this, ...args);
       }
+    });
+    ['insert', 'upsert', 'update', 'remove'].forEach(method => {
+      // TODO
     });
   });
 }
