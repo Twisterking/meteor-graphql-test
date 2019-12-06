62daf02c6783602bc1baac2f7690cae036148406
Patrick Schubert <patrick.schubert1@gmail.com>
2019-12-06 16:27:56 +0100

    I think wora works! good job morrys!

--- client/main.jsx
+++ client/main.jsx
@@ -5,12 +5,12 @@ import React from 'react';
 import gql from 'graphql-tag';
 window.gql = gql;
 
-import ApolloClient from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+// import ApolloClient from 'apollo-client';
+// import { InMemoryCache } from 'apollo-cache-inmemory';
 
 // WORA:
-// import { ApolloClient } from "@wora/apollo-offline";
-// import ApolloCache from '@wora/apollo-cache';
+import { ApolloClient } from "@wora/apollo-offline";
+import ApolloCache from '@wora/apollo-cache';
 
 import QueueLink from 'apollo-link-queue';
 import { RetryLink } from 'apollo-link-retry';
@@ -19,19 +19,28 @@ import { ApolloLink } from 'apollo-link';
 import { DDPLink } from 'meteor/swydo:ddp-apollo';
 import { persistCache } from 'apollo-cache-persist';
 import { ApolloProvider } from 'react-apollo';
+const ddpLink = new DDPLink();
 
 // import { OfflineClient } from 'offix-client';
 
-import { onError } from 'apollo-link-error';
-const errorLink = onError(() => {
-  console.log('Caught Apollo Client Error');
-});
-const ddpLink = new DDPLink();
-const queueLink = new QueueLink();
 // https://www.apollographql.com/docs/link/links/retry/
 // https://medium.com/twostoryrobot/a-recipe-for-offline-support-in-react-apollo-571ad7e6f7f4
-const retryLink = new RetryLink({ attempts : { max : Infinity } });
-const serializingLink = new SerializingLink();
+// https://github.com/apollographql/apollo-link/tree/master/packages/apollo-link-retry
+// import { onError } from 'apollo-link-error';
+// const errorLink = onError(() => {
+//   console.log('Caught Apollo Client Error');
+// });
+// const queueLink = new QueueLink();
+// const retryLink = new RetryLink({ 
+//   attempts: {
+//     max: Infinity,
+//     retryIf: (error, _operation) => {
+//       console.log({ error, _operation });
+//       return !!error || !Meteor.status().connected
+//     }
+//   }
+// });
+// const serializingLink = new SerializingLink();
 
 import App from '/imports/ui/App';
 
@@ -47,21 +56,21 @@ _ = require('lodash');
 
 
 // Persistant offline cache https://github.com/apollographql/apollo-cache-persist
-export let client;
-const cache = new InMemoryCache();
-const storage = window.localStorage;
-const waitOnCache = persistCache({ cache, storage });
+// export let client;
+// const cache = new InMemoryCache();
+// const storage = window.localStorage;
+// const waitOnCache = persistCache({ cache, storage });
 
 
 
 // WORA: https://morrys.github.io/wora/docs/apollo-offline
-// const cache = new ApolloCache({
-//   dataIdFromObject: o => o.id
-// });
-// export const client = new ApolloClient({
-//   link: ddpLink,
-//   cache
-// });
+const cache = new ApolloCache({
+  dataIdFromObject: o => o.id
+});
+export const client = new ApolloClient({
+  link: ddpLink,
+  cache
+});
 
 // client.setOfflineOptions({
 //   link: ddpLink
@@ -69,18 +78,19 @@ const waitOnCache = persistCache({ cache, storage });
 
 Meteor.startup(async () => {
   // wora
-  // await cache.hydrate();
+  await client.hydrate();
 
-  await waitOnCache;
-  client = new ApolloClient({
-    cache,
-    link: ApolloLink.from([
-      errorLink,
-      queueLink,
-      serializingLink,
-      ddpLink
-    ])
-  });
+  // await waitOnCache;
+  // client = new ApolloClient({
+  //   cache,
+  //   link: ApolloLink.from([
+  //     errorLink,
+  //     retryLink,
+  //     queueLink,
+  //     serializingLink,
+  //     ddpLink
+  //   ])
+  // });
   window.client = client;
 
   // fake offline:
